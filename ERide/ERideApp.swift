1471e4509e0f9b6495960599059aabcd482848f6
McMaurice <jachimzy@gmail.com>
2024-03-11 14:54:53 +0100

    Initial Commit

---
+++ ERide/ERideApp.swift
@@ -0,0 +1,17 @@
+//
+//  ERideApp.swift
+//  ERide
+//
+//  Created by Macmaurice Osuji on 3/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ERideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
